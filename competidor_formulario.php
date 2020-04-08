49d4fcfc712339180074c53877d8b4ca37be3529
Antonio2901 <antoniowellington542@gmail.com>
2020-04-08 01:29:58 -0300

    Adicionado  mensagens de retorno de validação

--- competidor_formulario.php
+++ competidor_formulario.php
@@ -1,18 +1,32 @@
+<?php 
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-	<meta charset="utf-8">
-	<title>Formulário de inscrição</title>
-	<meta name="author" content="">
-	<meta  name="description" content="">
-	<meta  name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-	<h1>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</h1>
-	<form action="dados.php" method="post">
-		<input type="text" name="nome" placeholder="Seu Nome">
-		<input type="text" name="idade" placeholder="Sua Idade">
-		<button type="submit" name="btn-enviar">Enviar</button>
-	</form>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>Formulário de inscrição</title>
+		<meta name="author" content="">
+		<meta  name="description" content="">
+		<meta  name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+<?php 
+	$erro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : "";
+	$sucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : "";
+	if(!empty($erro)){
+		echo "<li>".$erro."</li><hr>";
+		session_unset();
+	}elseif(!empty($sucesso)){
+		echo "<li>".$sucesso."</li><hr>";
+		session_unset();
+	}
+?>
+		<h1>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</h1>
+		<form action="dados.php" method="post">
+			<input type="text" name="nome" placeholder="Seu Nome">
+			<input type="text" name="idade" placeholder="Sua Idade">
+			<button type="submit" name="btn-enviar">Enviar</button>
+		</form>
+	</body>
 </html>
--- dados.php
+++ dados.php
@@ -1,5 +1,5 @@
 <?php
-	$erros = [];
+	session_start();
 	$categoria = [];
 	$categoria[] = "Infantil";
 	$categoria[] = "Adolescente";
@@ -11,46 +11,49 @@
 	$idade = filter_var($_POST['idade'], FILTER_VALIDATE_INT);
 	
 	if(empty($nome)){
-		$erros[] = "O campo Nome não pode estar vazio";
+		$_SESSION['mensagem-de-erro'] = "O campo Nome não pode estar vazio";
+		header('Location: index.php?dados=erro');
 	}else{
 		if(strlen($nome) <3 || strlen($nome) > 40){
-			$erros[] =  "O Nome tem que estar entre 3 e 40 caracteres";
+			$_SESSION['mensagem-de-erro'] =  "O Nome tem que estar entre 3 e 40 caracteres";
+			header('Location: index.php?dados=erro');
 		}
 	}
 
 	if(empty($idade)){
-		$erros[] = "O campo Idade só aceita numeros ou não pode estar vazio";
+		$_SESSION['mensagem-de-erro'] = "O campo Idade só aceita numeros ou não pode estar vazio";
+		header('Location: index.php?dados=erro');
 	}else{
-		if(strlen($idade) <2){
-			$erros[] =  "O campo Idade não pode conter mais de 2 caracteres";
+		if(strlen($idade) > 3){
+			$_SESSION['mensagem-de-erro'] =  "O campo Idade não pode conter mais de 2 caracteres";
+			header('Location: index.php?dados=erro');
 		}
 	}
 
-	if(empty($erros)){
+	if(empty($_SESSION['mensagem-de-erro'] )){
 		if($idade >= 6 AND $idade <= 12){
 			for($i =0 ; $i <count($categoria); $i++){
 				if($categoria[$i] == "Infantil"){
-					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+					$_SESSION['mensagem-de-sucesso'] =  "O Competidor ".$nome." foi inscrito na categoria ".$categoria[$i]." com sucesso";
+					header('Location:  index.php?dados=sucesso');
 				}
 			}
 
 		}elseif($idade >= 13 AND $idade <= 18){
 			for($i =0 ; $i <count($categoria); $i++){
 				if($categoria[$i] == "Adolescente"){
-					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+					$_SESSION['mensagem-de-sucesso'] =  "O Competidor ".$nome." foi inscrito na categoria ".$categoria[$i]." com sucesso";
+					header('Location:  index.php?dados=sucesso');
 				}
 			}	
-		}else{
+		}elseif($idade > 18){
 			for($i =0 ; $i <count($categoria); $i++){
 				if($categoria[$i] == "Adulto"){
-					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+					$_SESSION['mensagem-de-sucesso'] =  "O Competidor ".$nome." foi inscrito na categoria ".$categoria[$i]." com sucesso";
+					header('Location:  index.php?dados=sucesso');
 				}
 			}
 		}
-	}else{
-		foreach ($erros as $e) {
-			echo $e."<br>";
-		}
 	}
-	?>
-<button type="submit" name="btn-voltar">Voltar</button>
+?>
+
