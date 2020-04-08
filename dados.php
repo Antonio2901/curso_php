06b76f8e35c663778c5021628697ee27d1557417
Antonio2901 <antoniowellington542@gmail.com>
2020-04-07 23:25:35 -0300

    Adicionando validação de campos no formulario

--- dados.php
+++ dados.php
@@ -1,4 +1,5 @@
 <?php
+	$erros = [];
 	$categoria = [];
 	$categoria[] = "Infantil";
 	$categoria[] = "Adolescente";
@@ -6,28 +7,50 @@
 	//$nome = "Antonio";
 	//$idade  = 21;
 
-	$nome = $_POST['nome'];
-	$idade = $_POST['idade'];
-
-	if($idade >= 6 AND $idade <= 12){
-		for($i =0 ; $i <count($categoria); $i++){
-			if($categoria[$i] == "Infantil"){
-				echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
-			}
-		}
-
-	}elseif($idade >= 13 AND $idade <= 18){
-		for($i =0 ; $i <count($categoria); $i++){
-			if($categoria[$i] == "Adolescente"){
-				echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
-			}
-		}	
+	$nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
+	$idade = filter_var($_POST['idade'], FILTER_VALIDATE_INT);
+	
+	if(empty($nome)){
+		$erros[] = "O campo Nome não pode estar vazio";
 	}else{
-		for($i =0 ; $i <count($categoria); $i++){
-			if($categoria[$i] == "Adulto"){
-				echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
-			}
+		if(strlen($nome) <3 || strlen($nome) > 40){
+			$erros[] =  "O Nome tem que estar entre 3 e 40 caracteres";
 		}
 	}
-?>
+
+	if(empty($idade)){
+		$erros[] = "O campo Idade só aceita numeros ou não pode estar vazio";
+	}else{
+		if(strlen($idade) <2){
+			$erros[] =  "O campo Idade não pode conter mais de 2 caracteres";
+		}
+	}
+
+	if(empty($erros)){
+		if($idade >= 6 AND $idade <= 12){
+			for($i =0 ; $i <count($categoria); $i++){
+				if($categoria[$i] == "Infantil"){
+					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+				}
+			}
+
+		}elseif($idade >= 13 AND $idade <= 18){
+			for($i =0 ; $i <count($categoria); $i++){
+				if($categoria[$i] == "Adolescente"){
+					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+				}
+			}	
+		}else{
+			for($i =0 ; $i <count($categoria); $i++){
+				if($categoria[$i] == "Adulto"){
+					echo "O competidor ".$nome." é da categoria ".$categoria[$i]."<br>";
+				}
+			}
+		}
+	}else{
+		foreach ($erros as $e) {
+			echo $e."<br>";
+		}
+	}
+	?>
 <button type="submit" name="btn-voltar">Voltar</button>
